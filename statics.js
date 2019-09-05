ccca7be1b9b7f60aa4366d0d6dad695bc66e5c99
Telat Kaya <telat@karatekin.edu.tr>
2019-09-05 17:00:39 +0300

    add fonts

--- statics.js
+++ statics.js
@@ -2,7 +2,7 @@ const copy = require('copy'),
   c = console.log
 
 copy(
-  './src/statics/**/*.*',
+  './src/statics/**/**/**/*.*',
   './public/',
   err => (err) ? c(err.message) : c('Archivos copiados con éxito')
 )
